e6b89517770b87f66a651247c263200bc8e43f34
Brith6 <bryann.yabi@epitech.eu>
2025-11-06 18:18:22 +0100

    test

--- app/main.c
+++ app/main.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-#include <unistd.h> // Pour la fonction sleep()
 
 int main() {
-    // Boucle infinie pour garder le conteneur en vie
-    while(1) {
-        printf("Hello, Whanos C !!\n");
-        // Important pour que la sortie apparaisse dans les logs K8s
-        fflush(stdout); 
-        // Fait une pause de 5 secondes
-        sleep(5);
-    }
-    return 0; // Cette ligne ne sera jamais atteinte
+    printf("Hello, Whanos C !\n");
+    return 0;
 }
