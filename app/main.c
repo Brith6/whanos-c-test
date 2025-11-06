ad52ce68770dce1e3f0b2031894a964da0b07ba1
Brith6 <bryann.yabi@epitech.eu>
2025-11-06 02:49:47 +0100

    fix: implement infinite loop to keep container alive with periodic greeting

--- app/main.c
+++ app/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <unistd.h> // Pour la fonction sleep()
 
 int main() {
-    printf("Hello, Whanos C !!\n");
-    return 0;
+    // Boucle infinie pour garder le conteneur en vie
+    while(1) {
+        printf("Hello, Whanos C !!\n");
+        // Important pour que la sortie apparaisse dans les logs K8s
+        fflush(stdout); 
+        // Fait une pause de 5 secondes
+        sleep(5);
+    }
+    return 0; // Cette ligne ne sera jamais atteinte
 }
